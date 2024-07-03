fd7afa806032bcbce9ce8973dea931bd2c4225e2
rezar <113137577+ryzkyam@users.noreply.github.com>
2024-07-03 08:58:44 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './component/Header'
+import Footer from './component/Footer'
+import Hero from './pages/Hero'
+import Cars from './pages/Cars'
+import RentalForm from './pages/RentalForm'
+import Home from './pages/Home';
+import AboutCompany from './pages/AboutCompany';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <Header />
+    <Routes>
+      <Route path="/Home" Component={Home} />
+      <Route path="/Hero" Component={Hero} />
+      <Route path="/AboutCompany" Component={AboutCompany} />
+      <Route path="/Cars" Component={Cars} />
+      <Route path="/RentalForm" Component={RentalForm} />
+    </Routes>
+    <Footer />
+  </div>
+  )
+}
+
+export default App
+
+{/* <>
+<Header/>
+ <Hero/>
+ <Cars/>
+ <RentalForm/>
+ <Footer/>
+ </> */}
